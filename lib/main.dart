d68a26cd941ab5c4dd8f6a387fe41d2d4cd5cd16
TheCre8tor <cre8tor.alexander@gmail.com>
2022-11-24 00:48:49 +0100

    Created the NewContactView widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-
-import 'package:flutter/services.dart';
-import "package:rxdart/rxdart.dart";
+import 'package:reactive_programming/screens/contact/new_contact_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -28,58 +24,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Stream<String> getNames({required String filePath}) {
-  final names = rootBundle.loadString(filePath);
-  return Stream.fromFuture(names).transform(const LineSplitter());
-}
-
-Stream<String> getAllNames() {
-  final catNames = getNames(filePath: "assets/texts/cats.txt");
-  final dogNames = getNames(filePath: "assets/texts/dogs.txt");
-  return catNames.concatWith([dogNames]).delay(const Duration(seconds: 3));
-}
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("concat with RxDart"),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: FutureBuilder<List<String>>(
-            future: getAllNames().toList(),
-            builder: ((context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                case ConnectionState.waiting:
-                case ConnectionState.active:
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                case ConnectionState.done:
-                  final names = snapshot.requireData;
-
-                  return ListView.separated(
-                    separatorBuilder: (_, __) => const Divider(
-                      color: Colors.black,
-                    ),
-                    itemCount: names.length,
-                    itemBuilder: (context, idx) {
-                      return ListTile(
-                        title: Text(names[idx]),
-                      );
-                    },
-                  );
-              }
-            }),
-          ),
-        ),
-      ),
+    return NewContactView(
+      createContact: (firstName, lastName, phoneNumber) {},
+      goBack: () {},
     );
   }
 }
